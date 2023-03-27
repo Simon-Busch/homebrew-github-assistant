d1721fb5dee7742e5cf82eafb124ee59ec70f28b
simon busch <simon.busch@live.fr>
2023-03-27 22:31:06 +0200

    fix version

--- Formula/github-assistant.rb
+++ Formula/github-assistant.rb
@@ -3,7 +3,7 @@ class GithubAssistant < Formula
   homepage "https://github.com/Simon-Busch/github-assistant"
   url "https://github.com/Simon-Busch/github-assistant/releases/download/v0.1.2/github-assistant-mac.tar.gz"
   sha256 "74b19e8e74381c75fd8f8bdc06ac97411b08e83550be4dcbf599457ef9bcabf0"
-  version "0.1.1"
+  version "0.1.2"
 
   def install
     bin.install "github-assistant"
