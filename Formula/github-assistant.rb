4c0eb1a1211bb4e8ac982fda9412830b8ccd722f
simon busch <simon.busch@live.fr>
2023-03-28 16:26:12 +0200

    update sha

--- Formula/github-assistant.rb
+++ Formula/github-assistant.rb
@@ -2,7 +2,7 @@ class GithubAssistant < Formula
   desc "Keep track of your github assignments"
   homepage "https://github.com/Simon-Busch/github-assistant"
   url "https://github.com/Simon-Busch/github-assistant/releases/download/v0.1.2/github-assistant-mac.tar.gz"
-  sha256 "74b19e8e74381c75fd8f8bdc06ac97411b08e83550be4dcbf599457ef9bcabf0"
+  sha256 "10444335847d118f0540869630b0263b17df1ab1091d4f26cd115c25e300ff73"
   version "0.1.2"
 
   def install
